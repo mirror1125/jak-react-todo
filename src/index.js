3919db2764c47e926c4a2c282333c1d367f070f6
mirror1125 <mirrorseek@gmail.com>
2022-07-29 09:57:23 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import { App } from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
